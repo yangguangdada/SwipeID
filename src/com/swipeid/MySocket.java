278bec3f2f1226849f09ddb0fe39b0e8a1577054
LingqiFu <133837325+LingqiFu@users.noreply.github.com>
2023-06-07 16:32:20 +0800

    client

--- src/com/swipeid/MySocket.java
+++ src/com/swipeid/MySocket.java
@@ -8,34 +8,33 @@ import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Random;
 
 public class MySocket {
     private static final String ip="192.168.56.1";
-    private static final int port=9999;
+    private static final int port=8080;
     private static final int reg_data = 1;
     private static final int reg_request = 2;
     private static final int auth_data = 3;
     private static final int auth_request = 4;
-    public static void Register(String path,Context context) throws IOException {
+    private static final int id_range = 1000000;
+    public static void Register(String path,Context context,int type) throws IOException {
         if(Build.VERSION.SDK_INT>8){
             StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
         //注册
         try{
-            SendData(path,reg_data);
-            if(Request(reg_request)) Toast.makeText(context,"注册成功！",Toast.LENGTH_SHORT).show();
+            SendData(path,reg_data,type);
+            if(Request(reg_request,type)) Toast.makeText(context,"注册成功！",Toast.LENGTH_SHORT).show();
             else Toast.makeText(context,"注册失败！",Toast.LENGTH_SHORT).show();
         }catch (UnknownHostException e){
             e.printStackTrace();
@@ -44,15 +43,15 @@ public class MySocket {
             e.printStackTrace();
             Log.e(TAG, ("Register:" + e.getMessage()));}
     }
-    public static void Authentic(String path,Context context) throws IOException {
+    public static void Authentic(String path,Context context,int type) throws IOException {
         if(Build.VERSION.SDK_INT>8){
             StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
         //认证
         try{
-            SendData(path,auth_data);
-            if(Request(auth_request)) Toast.makeText(context,"认证成功！",Toast.LENGTH_SHORT).show();
+            SendData(path,auth_data,type);
+            if(Request(auth_request,type)) Toast.makeText(context,"认证成功！",Toast.LENGTH_SHORT).show();
             else Toast.makeText(context,"认证失败！",Toast.LENGTH_SHORT).show();
         }catch (UnknownHostException e){
             e.printStackTrace();
@@ -61,21 +60,23 @@ public class MySocket {
             e.printStackTrace();
             Log.e(TAG, ("Authentic:" + e.getMessage()));}
     }
-    private static void SendData(String path,int func) throws UnknownHostException,IOException{
+    private static void SendData(String path,int func,int type) throws UnknownHostException,IOException{
         File folder = new File(path);
-        File[] files = folder.listFiles();//存储所有文件
+        File[] files = folder.listFiles();//用数组存储所有文件
         try{
             Socket socket=new Socket(ip,port);
             OutputStream out = socket.getOutputStream();
-            BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(out));
-            BufferedReader reader =new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream writer=new DataOutputStream(out);
+            DataInputStream reader =new DataInputStream(socket.getInputStream());
             for (File file : files) {//遍历文件夹下文件并发送
                 if (file.isFile()) {
-                    writer.write(func);
+                    //写入认证文件信息
+                    writer.writeInt(func);
+                    writer.writeInt(type);
                     String filename = file.getName();
-                    writer.write(filename);
+                    writer.writeUTF(filename);
                     long filelength=file.length();
-                    writer.write((int) filelength);
+                    writer.writeInt((int) filelength);
                     //写入认证文件数据
                     FileInputStream inputStream = new FileInputStream(file);
                     byte[] buff = new byte[1024];
@@ -86,6 +87,7 @@ public class MySocket {
                     inputStream.close();
                 }
             }
+            //关闭连接
             writer.flush();
             writer.close();
             socket.close();
@@ -97,26 +99,28 @@ public class MySocket {
             Log.e(TAG, ("SendData:" + e.getMessage()));
         }
     }
-    private static boolean Request(int func) throws UnknownHostException,IOException {
+    private static boolean Request(int func,int type) throws UnknownHostException,IOException {
         final String username = "lxq";
         try{
-        Socket socket=new Socket(ip,port);
-        BufferedWriter writer=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        BufferedReader reader =new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        //写入认证信息
-        writer.write(func);
-        writer.write(username);
-        Random rand=new Random();
-        int id = rand.nextInt(1000000);
-        writer.write(String.valueOf(id));
-        //读取服务器返回数据包
-        int type = reader.read();
-        int result = reader.read();
-        int ackid = reader.read();
-        writer.flush();
-        writer.close();
-        socket.close();
-        if(type == 5 && result == 1 && ackid == id) return true;
+            Socket socket=new Socket(ip,port);
+            DataOutputStream writer =new DataOutputStream(socket.getOutputStream());
+            DataInputStream reader =new DataInputStream(socket.getInputStream());
+            //写入认证信息
+            writer.writeInt(func);
+            writer.writeInt(type);
+            writer.writeUTF(username);
+            Random rand=new Random();
+            int id = rand.nextInt(id_range);
+            writer.writeInt(id);
+            //读取服务器返回数据包
+            int Type = reader.read();
+            int result = reader.read();
+            int ackid = reader.read();
+            //关闭连接
+            writer.flush();
+            writer.close();
+            socket.close();
+            if(Type == 5 && result == 1 && ackid == id) return true;
         }catch (UnknownHostException e){
             e.printStackTrace();
             Log.e(TAG, ("Request:" + e.getMessage()));
